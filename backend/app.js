b7316578d30139e34c3d4d6501080cf5726bbe9f
sim1222 <kousuke11236124@gmail.com>
2021-10-02 01:07:55 +0900

    wip

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,9 @@ app.use(function(err, req, res, next) {
 
 app.use((req, res, next)=> {
   res.header('Access-Control-Allow-Origin', '*');
-  
-})
+  res.header(`Access-Control-Allow-Headers`, `Origin, X-Requested-With, Content-Type, Accept`);
+  res.header(`Access-Control-Allow-Methods`, `GET, POST, PUT, DELETE, OPTIONS`);
+  next();
+});
 
 module.exports = app;
